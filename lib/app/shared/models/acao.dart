cdca3edf29bec85bff29c932f7e3fac30c08d7ea
rdelonzek <renidelonzek@gmail.com>
2020-09-08 22:31:57 -0300

    fix: correção comentário

--- lib/app/shared/models/acao.dart
+++ lib/app/shared/models/acao.dart
@@ -3,8 +3,8 @@ import 'package:flutter/widgets.dart';
 typedef Funcao = void Function({dynamic data});
 
 class Acao {
-  Map<String, String>
-      chaves; //keys das colunas a serem enviadas, e o nome como elas devem ir
+  /// keys das colunas a serem enviadas, e o nome como elas devem ir
+  Map<String, String> chaves;
   String descricao;
   WidgetBuilder route;
   bool edicao;
